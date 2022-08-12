8f0d68371e197d27997d3e438b10a817966b8280
GoGaGTR <gogagtr@gmail.com>
2022-08-12 23:03:43 +0300

    Second commit

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -7,11 +7,11 @@ import static org.junit.jupiter.api.Assertions.*;
 public class StatisticsServiceTest {
 
     @Test
-    void findMax() {
+    void findMaxVer2() {
         StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 12;
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 13, 8, 6, 11, 11, 12};
+        long expected = 13;
 
         long actual = service.findMax(incomesInBillions);
 
